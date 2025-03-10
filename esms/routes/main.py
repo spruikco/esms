d100d1d8cc0b8697e34d7b4a843d934516b8ba14
Rye Smith <spruik.au@gmail.com>
2025-03-10 12:18:48 +1100

    Syncing repo: removing obsolete files and adding new changes

--- esms/routes/main.py
+++ esms/routes/main.py
@@ -1,11 +1,5 @@
 # esms/routes/main.py
-from flask import Blueprint, render_template, current_app
-
-main_bp = Blueprint('main', __name__)
-
-@main_bp.route('/')
-def index():
-    """Home page route"""
-    # For now, just render the index template without database calls
-    # This avoids dependencies on models that might not be implemented yet
-    return render_template('index.html')
+@main_bp.route('/league')
+def league():
+    """Placeholder for future league functionality"""
+    return render_template('placeholder.html', message="League functionality coming soon!")
--- esms/routes/match.py
+++ esms/routes/match.py
@@ -1,10 +1,15 @@
 # esms/routes/match.py
-from flask import Blueprint, render_template
+@match_bp.route('/results')
+def list_results():
+    """List match results"""
+    return render_template('placeholder.html', message="Match results will be shown here")
 
-match_bp = Blueprint('match', __name__)
+@match_bp.route('/sample')
+def sample():
+    """Run a sample match"""
+    return render_template('placeholder.html', message="Sample match functionality coming soon")
 
-@match_bp.route('/')
-def list_matches():
-    """View all matches"""
-    # This will be implemented later
-    return render_template('placeholder.html', message="Match listing will be implemented here")
+@match_bp.route('/simulate', methods=['POST'])
+def simulate():
+    """Simulate a match"""
+    return render_template('placeholder.html', message="Match simulation functionality coming soon")
